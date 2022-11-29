4bae4310c1e3c9cc5d5ce979ecff26510aed9284
Cory W <12cwarden@gmail.com>
2022-11-29 15:48:22 -0500

    added fix for import.sql showing accounts

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,7 @@
 INSERT INTO users (id, email, password, first_name, last_name, address, state, city, zip, user_type, creation_date) VALUES (1,'testuser@gmail.com','password', 'Bryan', 'Serfozo', '1234 Tampa Ave', 'Florida', 'Tampa', 57624, 'CLIENT', '2022-08-25T20:32:26.568Z');
+INSERT INTO users (id, email, password, first_name, last_name, address, state, city, zip, user_type, creation_date) VALUES (2,'test@gmail.com','pass', 'C', 'W', '1234 A Ave', 'Florida', 'Tampa', 57624, 'CLIENT', '2022-08-25T20:32:26.568Z');
 
 INSERT INTO accounts (id, name, balance, creation_date, user_id, account_type) VALUES (1,'Primary Checking',10000.00,'2022-08-25T20:32:26.568Z',1,'CHECKING');
-INSERT INTO accounts (id, name, balance, creation_date, user_id, account_type) VALUES (2,'Primary Savings',10000.00,'2022-08-25T20:32:26.568Z',1,'SAVINGS');
+INSERT INTO accounts (id, name, balance, creation_date, user_id, account_type) VALUES (2,'Primary Savings',10000.00,'2022-08-25T20:32:26.568Z',2,'SAVINGS');
 
 INSERT INTO transactions (id, amount, creation_date, description, type, sender_account_id, receiver_account_id) VALUES (1,2500.00,'2022-08-25T20:32:26.568Z','Payroll Direct Deposit','Income',1,2),(2,2500.00,'2022-08-25T20:32:26.568Z','Payroll Direct Deposit','Income',1,2),(3,2500.00,'2022-08-25T20:32:26.568Z','Payroll Direct Deposit','Income',1,2),(4,2500.00,'2022-08-25T20:32:26.568Z','Payroll Direct Deposit','Income',1,2);
