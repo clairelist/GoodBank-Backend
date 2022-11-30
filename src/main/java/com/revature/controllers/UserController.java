f742628e0500b96a9261926916e18c2362a90a98
clairelist <claire.list2501@gmail.com>
2022-11-30 11:09:52 -0800

    its LUNCHh time

---
+++ src/main/java/com/revature/controllers/UserController.java
@@ -0,0 +1,31 @@
+package com.revature.controllers;
+
+import com.revature.models.User;
+import com.revature.services.AuthService;
+import com.revature.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000", "http://good-bank-ui.s3-website-us-west-2.amazonaws.com"}, allowCredentials = "true")
+public class UserController {
+    private final UserService us;
+
+    public UserController(UserService us) {
+        this.us = us;
+    }
+
+    @PatchMapping("/{id}/reset-password")
+    public ResponseEntity<String> resetPass(@PathVariable("id") int id, @RequestBody String update){
+        //TODO: MAY HAVE TO CHANGE WHAT WE RETURN
+        //us.update(update) or some shit
+        return ResponseEntity.ok(update);
+    }
+}
