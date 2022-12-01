d25783d2ecd9361b8a74d586a055275be54da700
clairelist <claire.list2501@gmail.com>
2022-12-01 09:47:12 -0800

    some testing stuff yayyyy

--- src/test/java/com/revature/services/UserServiceTest.java
+++ src/test/java/com/revature/services/UserServiceTest.java
@@ -1,8 +1,30 @@
 package com.revature.services;
 
-//IAM THE UNIT TESTS FOR THE USER SERVICE!
+import com.revature.BankingApplication;
+import com.revature.models.User;
+import com.revature.repositories.UserRepository;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest(classes = BankingApplication.class)
 public class UserServiceTest {
 
-    //TODO:: TEST PATCH REQUEST WORKS
+    @MockBean
+    private UserRepository ur;
+
+    @Autowired
+    private UserService us;
+
+    @Test
+    public void updatePasswordPerformsUpdate(){
+        //TODO: I SHOULD FOLLOW THE SAME LOGIC AS UPDATING A RECIPE FROM BEFORE.
+    }
 }
