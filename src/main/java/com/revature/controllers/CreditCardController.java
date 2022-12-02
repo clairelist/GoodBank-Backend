6b857469909f9c566193d7ab1eded632f326c380
Cory W <12cwarden@gmail.com>
2022-12-02 18:04:44 -0500

    updating controllers with all headers accepted. doing in dev as group decision

--- src/main/java/com/revature/controllers/CreditCardController.java
+++ src/main/java/com/revature/controllers/CreditCardController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/credit-card")
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000", "http://good-bank-ui.s3-website-us-west-2.amazonaws.com"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000", "http://good-bank-ui.s3-website-us-west-2.amazonaws.com"}, allowCredentials = "true", allowedHeaders = "*")
 public class CreditCardController {
 
     @Autowired
