0689b5e8db1f6e1817e50461b21ba9f554258cde
clairelist <claire.list2501@gmail.com>
2022-12-02 11:51:02 -0800

    sonar cloud are you happy now

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -29,7 +29,7 @@ public class UserController {
             }
 
         } catch(Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
         }
     return response;
 
