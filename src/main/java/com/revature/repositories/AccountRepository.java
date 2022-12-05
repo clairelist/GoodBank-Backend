657a7ae3b53046e454db3483ac6d7eacf876a567
Patrick Grady <patrickrgrady81@gmail.com>
2022-12-05 11:56:58 -0500

    3 more code smells

--- src/main/java/com/revature/repositories/AccountRepository.java
+++ src/main/java/com/revature/repositories/AccountRepository.java
@@ -1,20 +1,14 @@
 package com.revature.repositories;
 
 import com.revature.models.Account;
-import com.revature.models.Transaction;
 import com.revature.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
-
-
-//    Optional<List<Account>> findByUser(User user);
 
     List<Account> findByUser(User user);
 
