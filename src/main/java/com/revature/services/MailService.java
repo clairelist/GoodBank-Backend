00dee8211be8a508bb8faf65988ac8ab99b8ef9b
clairelist <claire.list2501@gmail.com>
2023-03-17 15:48:20 -0600

    fk you java

--- src/main/java/com/revature/services/MailService.java
+++ src/main/java/com/revature/services/MailService.java
@@ -4,17 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; //below passwordEncoder is ABSTRACT, need THIS to instantiate
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class MailService {
 
-    @Autowired
-    public void sendMail(String recipient, int id){
-        MailSender ms = null;
-        String sender = "donotreply@goodbank.com";
-        String body = "You recently requested a password reset. Please click on this link (or copy and paste into your web browser) to reset your password. \n https://url-to-client-app/" + id;
+    private PasswordEncoder encoder;
 
-        //TODO: HASH THE ABOVE USERID!!!!
+    @Autowired
+    public void sendMail(String recipient, CharSequence id){
+        MailSender ms = null;
+        encoder = new BCryptPasswordEncoder();
+        String sender = "donotreply@goodbank.com";
+
+        String uid = encoder.encode(id);
+
+        String body = "You recently requested a password reset. Please click on this link (or copy and paste into your web browser) to reset your password. \n https://url-to-client-app/" + uid;
 
         SimpleMailMessage simp = new SimpleMailMessage();
         simp.setTo(recipient);
