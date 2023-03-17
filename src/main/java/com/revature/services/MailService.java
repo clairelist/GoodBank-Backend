82e5c27eb3458afce884bab5a6fe735650073260
clairelist <claire.list2501@gmail.com>
2023-03-17 15:22:11 -0600

    some done

--- src/main/java/com/revature/services/MailService.java
+++ src/main/java/com/revature/services/MailService.java
@@ -1,5 +1,27 @@
 package com.revature.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MailService {
-    //I think I EXTEND javamail or something... digging...
+
+    @Autowired
+    public void sendMail(String recipient, int id){
+        MailSender ms = null;
+        String sender = "donotreply@goodbank.com";
+        String body = "You recently requested a password reset. Please click on this link (or copy and paste into your web browser) to reset your password. \n https://url-to-client-app/" + id;
+
+        //TODO: HASH THE ABOVE USERID!!!!
+
+        SimpleMailMessage simp = new SimpleMailMessage();
+        simp.setTo(recipient);
+        simp.setFrom(sender);
+        simp.setSubject("Your GoodBank password reset");
+        simp.setText(body);
+
+        ms.send(simp);
+    }
 }
